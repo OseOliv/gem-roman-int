7b424135c6a696d377f1db71d1e7f8020f765de3
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-29 11:53:28 -0300

    feat: add basic functionality for roman to int

--- lib/roman_to_int.rb
+++ lib/roman_to_int.rb
@@ -4,5 +4,29 @@ require_relative "roman_to_int/version"
 
 module RomanToInt
   class Error < StandardError; end
-  # Your code goes here...
-end
+  class RomanToInteger
+    def self.roman_to_int(s)
+      roman_hash = {
+        'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50,
+        'C' => 100, 'D' => 500, 'M' => 1000
+      }
+
+      total = 0
+      prev_value = 0
+
+      s.upcase.chars.reverse.each do |char|
+        value = roman_hash[char]
+        raise ArgumentError, "Invalid Roman numeral character" if value.nil?
+
+        if value < prev_value
+          total -= value
+        else
+          total += value
+        end
+        prev_value = value
+      end
+
+      total
+    end
+  end
+  end
