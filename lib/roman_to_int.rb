29545e274f1096e12d3eb0f9dd057e18026c606f
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-29 11:37:49 -0300

    chore: initialize gem structure

---
+++ lib/roman_to_int.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "roman_to_int/version"
+
+module RomanToInt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/roman_to_int/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RomanToInt
+  VERSION = "0.1.0"
+end
