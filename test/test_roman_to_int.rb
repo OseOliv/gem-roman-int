d4780123537881c4df7e925a77144e2c82147906
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-29 11:53:46 -0300

    test: add initial tests for roman to int

--- test/test_roman_to_int.rb
+++ test/test_roman_to_int.rb
@@ -7,7 +7,17 @@ class TestRomanToInt < Minitest::Test
     refute_nil ::RomanToInt::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_roman_to_int
+    assert_equal 3, RomanToInt::RomanToInteger.roman_to_int("III")
+    assert_equal 4, RomanToInt::RomanToInteger.roman_to_int("IV")
+    assert_equal 9, RomanToInt::RomanToInteger.roman_to_int("IX")
+    assert_equal 58, RomanToInt::RomanToInteger.roman_to_int("LVIII")
+    assert_equal 1994, RomanToInt::RomanToInteger.roman_to_int("MCMXCIV")
+  end
+
+  def test_invalid_roman_numeral
+    assert_raises(ArgumentError) do
+      RomanToInt::RomanToInteger.roman_to_int("ABC")
+    end
   end
 end
